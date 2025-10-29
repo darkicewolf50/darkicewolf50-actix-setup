6a87c2c4139d05b192a48b25dd45b6f9fb4a65aa
darkicewolf50 <darkicewolf50@gmail.com>
2025-10-28 21:35:11 -0600

    init(master)

---
+++ README.md
@@ -0,0 +1,13 @@
+# darkicewolf50-actix-setup
+
+Includes a health check function, and a logger
+
+![Workflow](https://forge.ucalgarybaja.ca/darkicewolf50/darkicewolf50-actix-setup/badges/workflows/all_tests.yaml/badge.svg?branch=master)
+![Open Issues](https://forge.ucalgarybaja.ca/darkicewolf50/darkicewolf50-actix-setup/badges/issues/open.svg)
+![GitHub stars](https://img.shields.io/github/stars/darkicewolf50/darkicewolf50-actix-setup?style=flat-square)
+
+# Features
+
+log_incoming, logs the two strings to the terminal, is useful for knowing what was polled in seting and what was polled in the production
+
+health_check, sends back a json stating that the server is alive
---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+
+/// Logs out the request to the application, with method, and path it took to get there
+///
+/// # Params
+///
+/// - method - One of the HTTP request mothds.
+/// - path_source - the path you need to get to the function
+///
+/// # Returns
+///
+/// - Nothing, prints to terminal the method used and path it is going to
+///
+/// # Example
+/// ```rust
+/// // this is how a public but internal module would be used by an outside user (ex_crate needs to be changed)
+/// use ucalg_baja_cloud::log_incoming;
+/// let result = log_incoming("GET", "/");
+/// // unit value and should only be printed to the terminal
+/// assert_eq!(result, ())
+/// ```
+/// # Author (s)
+///
+/// - Brock <brock@darkicewolf50.dev>
+
+pub fn log_incoming(method: &'static str, path_source: &str) {
+    println!("{} request, path: {}", method, path_source);
+}
+
+/// A quick method to check if the server is alive and running
+/// This also keeps out scrapers from getting useful data
+///
+/// # Params
+///
+/// - nothing - needs nothing to check health of server
+///
+/// # Returns
+///
+/// - Json response with a alive message
+///
+/// # Example
+/// ```rust
+/// use ucalg_baja_cloud::hello;
+/// use actix_web::{web, test, App};
+/// use serde_json::json;
+///
+/// #[actix_web::test]
+/// async fn test_hello() {
+///     let app = test::init_service(App::new().service(hello)).await;
+///     let req = test::TestRequest::get().uri("/").to_request();
+///     let resp: serde_json::Value = test::call_and_read_body_json(&app, req).await;
+///
+///     assert_eq!(resp, json!({
+///         "body": {
+///             "message": "Hello I am alive, this does nothing"
+///         }
+///     }));
+/// }
+/// ```
+/// # Author (s)
+///
+/// - Brock <brock@darkicewolf50.dev>
+
+#[get("/")]
+pub async fn health_check() -> impl Responder {
+    log_incoming("GET", "/");
+    web::Json(json!({
+    "body": {
+            "message": "Hello World! I am alive, this does nothing"
+        }
+    }))
+}
