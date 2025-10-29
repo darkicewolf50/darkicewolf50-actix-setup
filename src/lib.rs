d1c72569a8a80480a93624c2f4fe6d118383b29b
darkicewolf50 <darkicewolf50@gmail.com>
2025-10-29 13:29:57 -0600

    feat(master): cleaner for file requests, removed auto publisher

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use actix_web::{Responder, get, web};
-
+use actix_web::{HttpResponse, Responder, get, web};
+use regex::Regex;
 use serde_json::json;
+use std::path::{Path, PathBuf};
+use unicode_normalization::UnicodeNormalization;
+
 /// Logs out the request to the application, with method, and path it took to get there
 ///
 /// # Params
@@ -70,4 +73,67 @@ pub async fn health_check() -> impl Responder {
             "message": "Hello World! I am alive, this does nothing"
         }
     }))
+}
+
+/// Logs out the request to the application, with method, and path it took to get there
+///
+/// # Params
+///
+/// - method - One of the HTTP request mothds.
+/// - path_source - the path you need to get to the function
+///
+/// # Returns
+///
+/// - Nothing, prints to terminal the method used and path it is going to
+///
+/// # Example
+/// ```rust
+/// use std::path::Path;
+/// use darkicewolf50_actix_setup::clean_file_user_path;
+///
+/// let result = clean_file_user_path("/database", "test".to_string(), "txt").unwrap();
+///
+/// assert_eq!(result, Path::new("/database/test.txt"))
+/// ```
+///
+/// ```rust
+/// use std::path::Path;
+/// use darkicewolf50_actix_setup::clean_file_user_path;
+/// let result = clean_file_user_path("/database", "test".to_string(), ".txt").unwrap();
+///
+/// assert_eq!(result, Path::new("/database/test.txt"))
+/// ```
+/// # Author (s)
+///
+/// - Brock <brock@darkicewolf50.dev>
+
+pub fn clean_user_file_req(
+    base_path: &str,
+    user_file_request: String,
+    file_extension: &str,
+) -> Result<PathBuf, HttpResponse> {
+    let normalized_file_req = user_file_request.nfc().collect::<String>();
+
+    if normalized_file_req.is_empty() || normalized_file_req.len() > 255 {
+        return Err(HttpResponse::BadRequest().body("invalid blog name"));
+    }
+    if normalized_file_req.chars().any(|c| c.is_control()) {
+        return Err(HttpResponse::BadRequest().body("invalid blog name"));
+    }
+
+    let traversal_regex = Regex::new(r"(\.\.|/|\\)").unwrap();
+    if traversal_regex.is_match(&normalized_file_req) {
+        return Err(HttpResponse::BadRequest().body("invalid blog name"));
+    }
+
+    let allowed_char = Regex::new(r"^[A-Za-z0-9 _\-\(\)\[\]]+$").unwrap();
+    if !allowed_char.is_match(&normalized_file_req) {
+        return Err(HttpResponse::BadRequest().body("invalid blog name"));
+    }
+
+    let final_path = Path::new(base_path)
+        .join(&normalized_file_req)
+        .with_extension(file_extension.trim_start_matches('.'));
+
+    Ok(final_path)
 }
